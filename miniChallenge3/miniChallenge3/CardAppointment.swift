6f2f60191324b8aede3edec34db48ea8498af50a
Julia <smndsjulia@gmail.com>
2023-03-22 14:46:10 -0300

    Implementação do card consulta
    data, card e os dois juntos.

---
+++ miniChallenge3/miniChallenge3/CardAppointment.swift
@@ -0,0 +1,38 @@
+//
+//  CardConsulta.swift
+//  miniChallenge3
+//
+//  Created by Julia Mendes on 20/03/23.
+//
+
+import SwiftUI
+
+struct CardAppointment: View {
+    
+    private var specialist: String
+    private var doctor: String
+    private var hour: String
+    private var dateAppointment: String
+    
+    init(specialist: String, doctor: String, hour: String, dateAppointment: String) {
+        self.specialist = specialist
+        self.doctor = doctor
+        self.hour = hour
+        self.dateAppointment = dateAppointment
+    }
+    
+    var body: some View {
+        VStack(spacing: 0){
+            DateCard(dateAppointment: dateAppointment)
+            DetailCard(specialist: specialist, doctor: doctor, hour: hour)
+        }
+    }
+}
+
+
+
+struct CardConsulta_Previews: PreviewProvider {
+    static var previews: some View {
+        CardAppointment(specialist: "Otorrinolaringologista", doctor: "Gabriel", hour: "9h32", dateAppointment: "24 de março de 2024")
+    }
+}
---
+++ miniChallenge3/miniChallenge3/DateCard.swift
@@ -0,0 +1,43 @@
+//
+//  DataCard.swift
+//  miniChallenge3
+//
+//  Created by Julia Mendes on 21/03/23.
+//
+
+import SwiftUI
+
+struct DateCard: View {
+    
+    let radius = 20.0
+    private var dateAppointment: String
+    
+    init(dateAppointment: String) {
+        self.dateAppointment = dateAppointment
+    }
+    var body: some View {
+        GeometryReader { geometry in
+            Rectangle()
+                .fill(.blue)
+                .frame(width: geometry.size.width, height: 60)
+                .padding(.bottom, radius)
+                .cornerRadius(CGFloat(radius))
+            dateText
+        }
+        .padding()
+        .frame(width: .infinity, height: 60)
+    }
+    
+    var dateText: some View {
+        Text(dateAppointment)
+            .font(.title2)
+            .padding()
+    }
+    
+}
+
+struct DataCard_Previews: PreviewProvider {
+    static var previews: some View {
+        DateCard(dateAppointment: "24 de março de 2021")
+    }
+}
