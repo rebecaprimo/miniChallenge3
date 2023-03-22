e6d1bdc4d716befc8a8e19d5e812b68a3f90139e
Bruno.Lafayette <brunolafayette@rocketmail.com>
2023-03-22 14:19:03 -0300

    Criado tab bar personalizada

---
+++ miniChallenge3/miniChallenge3/components/ButtonTabBar.swift
@@ -0,0 +1,20 @@
+//
+//  ButtonTabBar.swift
+//  miniChallenge3
+//
+//  Created by Bruno Lafayette on 21/03/23.
+//
+
+import SwiftUI
+
+struct ButtonTabBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ButtonTabBar_Previews: PreviewProvider {
+    static var previews: some View {
+        ButtonTabBar()
+    }
+}
