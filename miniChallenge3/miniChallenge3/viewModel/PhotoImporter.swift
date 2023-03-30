3bc521369d37d0aa822afff63b0ba70c235c4b26
Julia <smndsjulia@gmail.com>
2023-03-30 14:31:58 -0300

    Adição de fotos usando PhotoUI

---
+++ miniChallenge3/miniChallenge3/viewModel/PhotoImporter.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoImporter.swift
+//  miniChallenge3
+//
+//  Created by Julia Mendes on 30/03/23.
+//
+
+import Foundation
+import SwiftUI
+import PhotosUI
+
+//TENTAR FAZER:
+//Puxar o pdf com o fileImporter
+//Usar o pdfdocument e instanciar do tipo pdfdocument que recebe uma url
+//https://developer.apple.com/documentation/pdfkit/pdfdocument
+//salvar como tipo data no coredata
+
