fe5a656375846c10eb32cec6ffbbd928dfa7f789
Rebeca Primo <rebecarodriguesprimo036@gmail.com>
2023-03-30 14:34:46 -0300

    splash view feita

--- miniChallenge3/miniChallenge3/miniChallenge3App.swift
+++ miniChallenge3/miniChallenge3/miniChallenge3App.swift
@@ -9,14 +9,15 @@ import SwiftUI
 
 @main
 struct miniChallenge3App: App {
-    let persistenceController = PersistenceController.shared
-    @State var selectedTab: String = "list.bullet.clipboard"
+//    let persistenceController = PersistenceController.shared
+//    @State var selectedTab: String = "list.bullet.clipboard"
 
     var body: some Scene {
         WindowGroup {
-            CustomTabBar(selectedTab: $selectedTab)
-                .ignoresSafeArea()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+            SplashView()
+//            CustomTabBar(selectedTab: $selectedTab)
+//                .ignoresSafeArea()
+//                .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
 }
